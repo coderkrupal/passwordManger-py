b741a9de2497f3109764b1fd4fd3da463465a85e
coderkrupall <krupaldoshi01@gmail.com>
2025-03-20 20:25:53 +0530

    prjectdone

--- Signin.py
+++ Signin.py
@@ -16,6 +16,18 @@ YELLOW = "\033[1;33m"
 MAGENTA = "\033[1;35m"
 RESET = "\033[0m"  # Reset to default color
 
+conn = sqlite3.connect("passwords.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+         CREATE TABLE IF NOT EXISTS password(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+         use_pass TEXT NOT NULL UNIQUE
+        )
+        """
+)
+
 
 def CreateStrongPassword():
     Npassword = int(input("enter a number of word > "))
@@ -23,7 +35,23 @@ def CreateStrongPassword():
     password = "".join(secrets.choice(characters) for _ in range(Npassword))
     os.system("cls" if os.name == "nt" else "clear")
     print(f"{GREEN}🔐 Strong Password:", password)
+    try:
+        cursor.execute("INSERT INTO password (use_pass) VALUES (?)", (password,))
 
+        conn.commit()
+        print("password addes successfully")
+    except sqlite3.Error as e:
+        print(f"❌ Database error: {e}")
+    finally:
+        conn.close()
+        
+    
+def webApppasswords():
+    web_password = input(f"{CYAN}enter application name you generate password >")
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(characters) for _ in range(10))
+    os.system("cls" if os.name == "nt" else "clear")
+    print(f"{GREEN}🔐 Strong web  Password:", password)
 
 def signin(email, password):
     try:
@@ -43,13 +71,11 @@ def signin(email, password):
             banner.colored_banner()
             print(f"{GREEN}1. Create strong password{RESET}")
             print(f"{BLUE}2. Web passwords{RESET}")
-            print(f"{CYAN}3. Search password{RESET}")
-            print(f"{YELLOW}4. Strong password list{RESET}")
             main_choice = int(input(f"{RED}enter choice > "))
             if main_choice == 1:
                 CreateStrongPassword()
             if main_choice == 2:
-                pass
+                 webApppasswords()
             if main_choice == 3:
                 pass
             if main_choice == 4:
